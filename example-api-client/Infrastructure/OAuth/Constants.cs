d2160f82ecafaa8cbf17b9286b09067143d74b78
Ethan <my4ourKs>
2016-03-05 15:06:20 -0600

    Initial repo upload

---
+++ example-api-client/Infrastructure/OAuth/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace example_api_client.Infrastructure.OAuth
+{
+    internal static class Constants
+    {
+        public const string DefaultAuthenticationType = "Stitchz";
+    }
+}
---
+++ example-api-client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(example_api_client.Startup))]
+namespace example_api_client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
